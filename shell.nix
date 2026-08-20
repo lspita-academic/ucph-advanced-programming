a02701f0dc144fb9dc9deb1ce9f7fe08db9b9d8a
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-08-20 23:47:04 +0200

    build(devshell): nix support

--- shell.nix
+++ shell.nix
@@ -2,21 +2,24 @@ let
   # Pinning nixpkgs for reproducibility. If you want to use your
   # system Nixpkgs, use the other definition of 'nixpkgs' that is
   # commented out below.
-  nixpkgs = builtins.fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/refs/tags/26.05.tar.gz;
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/26.05.tar.gz";
     sha256 = "sha256:0am8xx09fx5yf2p0wb001v0jx1g5hrfb76h4r37xph378jgk7pcr";
   };
   # nixpkgs = <nixpkgs>;
 
-  pkgs = import nixpkgs {};
+  pkgs = import nixpkgs { };
 in
 pkgs.stdenv.mkDerivation {
   name = "AP2026";
-  buildInputs =
-    with pkgs;
-    [
-      haskell.compiler.ghc912
-      cabal-install
-      haskell-language-server
-    ];
+  buildInputs = with pkgs; [
+    # nix
+    nixd
+    nil
+    nixfmt
+    # haskell
+    haskell.compiler.ghc912
+    cabal-install
+    # (haskell-language-server.override { supportedGhcVersions = [ “91” ]; })
+  ];
 }
