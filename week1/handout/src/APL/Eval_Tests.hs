9198d14fa7dcac7b3a950b448529d05dc8594a8b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-09-03 16:20:10 +0200

    feat(ex1): add constant int evaluation

--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,15 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+import APL.AST (Exp (CstInt))
+import APL.Eval (Val (ValInt), eval)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (assertBool, testCase)
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [ testCase "Evaluate constant ints" $ assertBool "should evaluate to int" $ ValInt 2 == eval (CstInt 2),
+      testCase "Evaluate negative constant ints" $ assertBool "should evaluate to negative int" $ ValInt (-2) == eval (CstInt (-2)),
+      testCase "Evaluate different constant ints" $ assertBool "should not be equal values" $ eval (CstInt 2) /= eval (CstInt 3)
+    ]
