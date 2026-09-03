00826abe9cd1bc768d6e716ae5c5fded2b0d6b7f
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-09-03 16:45:06 +0200

    naive eval operations case statement

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -4,11 +4,24 @@ module APL.Eval
   )
 where
 
-import APL.AST (Exp (CstInt))
+import APL.AST (Exp (Add, CstInt, Div, Mul, Pow, Sub))
 
 data Val
   = ValInt Integer
   deriving (Eq, Show)
 
+binaryEval :: Exp -> Exp -> (Val, Val)
+binaryEval x y = (eval x, eval y)
+
 eval :: Exp -> Val
 eval (CstInt n) = ValInt n
+eval (Add x y) = case binaryEval x y of
+  (ValInt x', ValInt y') -> ValInt (x' + y')
+eval (Sub x y) = case binaryEval x y of
+  (ValInt x', ValInt y') -> ValInt (x' - y')
+eval (Mul x y) = case binaryEval x y of
+  (ValInt x', ValInt y') -> ValInt (x' * y')
+eval (Div x y) = case binaryEval x y of
+  (ValInt x', ValInt y') -> ValInt (x' `div` y')
+eval (Pow x y) = case binaryEval x y of
+  (ValInt x', ValInt y') -> ValInt (x' ^ y')
