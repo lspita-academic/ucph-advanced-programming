b281b2a5359de550d47af0fd8ef2e04a94c06ef5
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-09-03 16:20:10 +0200

    feat(ex1): add constant int evaluation

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,14 @@
 module APL.Eval
-  (
+  ( Val (..),
+    eval,
   )
 where
 
-import APL.AST ()
+import APL.AST (Exp (CstInt))
+
+data Val
+  = ValInt Integer
+  deriving (Eq, Show)
+
+eval :: Exp -> Val
+eval (CstInt n) = ValInt n
