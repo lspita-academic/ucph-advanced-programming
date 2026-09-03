3ea91ce076ef1c67fc57cabb2a535986d12af026
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-09-03 17:21:40 +0200

    feat(test): add operations tests

--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,6 +1,6 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST (Exp (CstInt))
+import APL.AST (Exp (Add, CstInt, Div, Mul, Pow, Sub))
 import APL.Eval (Val (ValInt), eval)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (assertBool, testCase)
@@ -9,7 +9,18 @@ tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    [ testCase "Evaluate constant ints" $ assertBool "should evaluate to int" $ ValInt 2 == eval (CstInt 2),
-      testCase "Evaluate negative constant ints" $ assertBool "should evaluate to negative int" $ ValInt (-2) == eval (CstInt (-2)),
-      testCase "Evaluate different constant ints" $ assertBool "should not be equal values" $ eval (CstInt 2) /= eval (CstInt 3)
+    [ testGroup
+        "Constants evaluation"
+        [ testCase "Evaluate constant ints" $ assertBool "should evaluate to int" $ ValInt 2 == eval (CstInt 2),
+          testCase "Evaluate negative constant ints" $ assertBool "should evaluate to negative int" $ ValInt (-2) == eval (CstInt (-2)),
+          testCase "Evaluate different constant ints" $ assertBool "should not be equal values" $ eval (CstInt 2) /= eval (CstInt 3)
+        ],
+      testGroup
+        "Basic operations"
+        [ testCase "Evaluate addition" $ assertBool "should add two ints" $ ValInt 5 == eval (CstInt 2 `Add` CstInt 3),
+          testCase "Evaluate subtraction" $ assertBool "should subtract two ints" $ ValInt (-1) == eval (CstInt 2 `Sub` CstInt 3),
+          testCase "Evaluate multiplication" $ assertBool "should multiply two ints" $ ValInt 6 == eval (CstInt 2 `Mul` CstInt 3),
+          testCase "Evaluate division" $ assertBool "should divide two ints" $ ValInt 2 == eval (CstInt 6 `Div` CstInt 3),
+          testCase "Evaluate exponentiation" $ assertBool "should exponentiate two ints" $ ValInt 8 == eval (CstInt 2 `Pow` CstInt 3)
+        ]
     ]
