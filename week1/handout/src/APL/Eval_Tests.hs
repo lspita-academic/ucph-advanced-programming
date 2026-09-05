7477166ec0d40c50f1f38d710b7ce2bf855eabd5
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-09-05 13:33:59 +0200

    test(eval): test simple equality between constants

--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,7 +1,7 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST (Exp (Add, CstInt, Div, Mul, Pow, Sub, CstBool))
-import APL.Eval (EvalError (DivisionByZero, NegativeExponent), Val (ValInt, ValBool), eval)
+import APL.AST (Exp (Add, CstBool, CstInt, Div, Eql, Mul, Pow, Sub))
+import APL.Eval (EvalError (DivisionByZero, NegativeExponent), Val (ValBool, ValInt), eval)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (assertBool, testCase)
 
@@ -18,7 +18,7 @@ tests =
           testCase "Evaluate different constant booleans" $ assertBool "should not be equal values" $ eval (CstBool True) /= eval (CstBool False)
         ],
       testGroup
-        "Binary operations"
+        "Arithmetic operations"
         [ testCase "Evaluate addition" $ assertBool "should add two ints" $ Left (ValInt 5) == eval (CstInt 2 `Add` CstInt 3),
           testCase "Evaluate subtraction" $ assertBool "should subtract two ints" $ Left (ValInt (-1)) == eval (CstInt 2 `Sub` CstInt 3),
           testCase "Evaluate multiplication" $ assertBool "should multiply two ints" $ Left (ValInt 6) == eval (CstInt 2 `Mul` CstInt 3),
@@ -26,13 +26,15 @@ tests =
           testCase "Evaluate exponentiation" $ assertBool "should exponentiate two ints" $ Left (ValInt 8) == eval (CstInt 2 `Pow` CstInt 3)
         ],
       testGroup
-        "Binary operations failure"
+        "Arithmetic operations failure"
         [ testCase "Division by 0" $ assertBool "should return a division by 0 error" $ Right DivisionByZero == eval (CstInt 2 `Div` CstInt 0),
           testCase "Negative exponent" $ assertBool "should return a negative exponent error " $ Right NegativeExponent == eval (CstInt 2 `Pow` CstInt (-3))
         ],
       testGroup
-        "Conditionals operations failure"
-        [ testCase "Division by 0" $ assertBool "should return a division by 0 error" $ Right DivisionByZero == eval (CstInt 2 `Div` CstInt 0),
-          testCase "Negative exponent" $ assertBool "should return a negative exponent error " $ Right NegativeExponent == eval (CstInt 2 `Pow` CstInt (-3))
+        "Conditionals operations"
+        [ testCase "Evaluate integers equality" $ assertBool "integers should be equal" $ Left (ValBool True) == eval (CstInt 2 `Eql` CstInt 2),
+          testCase "Evaluate integers inequality" $ assertBool "integers should not be equal" $ Left (ValBool False) == eval (CstInt 3 `Eql` CstInt 2),
+          testCase "Evaluate booleans equality" $ assertBool "booleans should be equal" $ Left (ValBool True) == eval (CstBool True `Eql` CstBool True),
+          testCase "Evaluate booleans inequality" $ assertBool "booleans should not be equal" $ Left (ValBool False) == eval (CstBool True `Eql` CstBool False)
         ]
     ]
