7992115a7d0a72118f98647f9d32d566cf142820
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-09-05 10:45:56 +0200

    feat(eval): add conditions support + refactor error handling

--- week1/handout/src/APL/AST.hs
+++ week1/handout/src/APL/AST.hs
@@ -5,9 +5,12 @@ where
 
 data Exp
   = CstInt Integer
+  | CstBool Bool
   | Add Exp Exp
   | Sub Exp Exp
   | Mul Exp Exp
   | Div Exp Exp
   | Pow Exp Exp
+  | Eql Exp Exp
+  | If Exp Exp Exp
   deriving (Show, Eq)
